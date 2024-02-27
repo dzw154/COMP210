e063453375ac0ab5ab10711fad7372c5d3d9f00a
Daniel Wang <dzw@unc.edu>
2024-02-26 22:50:53 -0500

    Assn03 version 1

---
+++ src/assn03/LinkedList.java
@@ -0,0 +1,325 @@
+package assn03;
+
+public class LinkedList<T> {
+    private Node<T> head = null;
+    private Node<T> tail = null;
+    private int size = 0;
+
+
+    /**
+     * Task1
+     * Remove the node at index i of the list.
+     * Note that the first element is at index 0
+     * If i is larger than the size of the list, throw an IndexOutOfBounds Exception
+     *
+     * ex: list: A -> B -> C -> D
+     *     i: 1
+     *     list after removeAtIndex: A -> C -> D
+     *
+     * @param i    - index of node to remove
+     */
+    
+    public void removeAtIndex(int i) {
+        Node<T> current = head;
+        validIndex(i);
+        int j = 0;
+        while (j < i) {
+            j++;
+            current = current.getNext();
+        }
+        remove(current.getValue());
+    }
+
+
+
+    /**
+     * Task2
+     * Return true if this linked list is equal to the list argument, false otherwise.
+     * Two lists are equal if they have the same size, and the same
+     * elements in the same order.
+     * ex:  list: 1 -> 4 -> 2
+     *      list2: 1 -> 4 -> 2
+     *      return: true
+     *
+     *      list: 1 -> 5
+     *      list2: 2 -> 5
+     *      return false;
+     *
+     * @param list2 - the list to compare with the current list
+     * @return true if the lists have the same elements in the same order, false otherwise
+     */
+    public boolean isEqual(LinkedList list2) {
+        Node<T> current = head;
+        if (list2.size()!=size)
+            return false;
+        while (current != null){
+            if (!list2.contains(current)){
+                return false;
+            }
+            if (list2.indexOf(current)!=indexOf(current)){
+                return false;
+            }
+            current = current.getNext();
+        }
+        return true;
+    }
+
+
+
+    /**
+     * Task3
+     * Given a sorted linked list, remove the duplicate values from the list
+     * ex: list: 5 -> 6 -> 7 -> 7 -> 7 -> 8 -> 8 -> 9
+     *     list after removeRepeats: 5 -> 6 -> 7 -> 8 -> 9
+     *
+     */
+    public void removeRepeats() {
+        Node<T> current = head;
+        while ((current != null) && current.hasNext()){
+            Node<T> next = current.getNext();
+            if (current.getValue()==next.getValue()){
+                remove(current);
+            }
+        }
+    }
+
+
+
+
+    /**
+     * Task4
+     * Reverse the list
+     *
+     * ex list:  10 -> 9 -> 8 -> 7
+     *    list after reverse: 7 -> 8 -> 9 -> 10
+     *
+     */
+    public void reverse() {
+
+    }
+
+
+
+
+
+    /**
+     * Task5
+     * Merge the given linked list2 into the current list. The 2 lists will always be
+     * either the same size, or the current list will be longer than list2.
+     * The examples below show how to handle each case.
+     *
+     * Note: Do NOT create and return a new list, merge the second list into the first one.
+     *
+     * ex: list: 1 -> 2 -> 3
+     *     list2: 4 -> 5 -> 6
+     *     return: 1 -> 4 -> 2 -> 5 -> 3 -> 6
+     *
+     *     list: 1 -> 2 -> 3 -> 4
+     *     list2: 5 -> 6
+     *     return 1 -> 5 -> 2 -> 6 -> 3 -> 4
+     *
+     * @param list2
+     */
+    public void merge(LinkedList list2) {
+    }
+
+
+    /* Implementations below are being given to you. Do not modify below this. */
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public boolean contains(Object element) { // Checks if the linked list contains the given element
+        Node<T> current = head;
+        while(current != null) {
+            if(current.getValue().equals(element)) {
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+
+    public T[] toArray() { // Converts the linked list to an array
+        T[] arr =  (T[]) new Object[size()];
+        Node<T> current = head;
+        int i = 0;
+        if(isEmpty()) {
+            return arr;
+        }
+        while(current != null){
+            arr[i] = current.getValue();
+            current = current.getNext();
+            i++;
+        }
+        return arr;
+    }
+
+    public void add(Object element) { // Adds an object to the end of the linked list
+        Node<T> newNode = new NodeImpl<T>((T) element, null);
+        if(isEmpty()) {
+            head = newNode;
+            tail = newNode;
+            size++;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
+            size++;
+        }
+
+    }
+
+    public boolean remove(Object element) { // Removes the current object from the linked list
+        Node<T> current = head;
+        if(isEmpty()) {
+            return false;
+        }
+        if(current.getValue() == element){
+            head = head.getNext();
+            size--;
+            return true;
+        }
+        while(current.getNext().getValue() != element) {
+            current = current.getNext();
+            if(current == null) {
+                return false;
+            }
+        }
+        if(current.getNext().getNext() == null) {
+            tail = current;
+        }
+        current.setNext(current.getNext().getNext());
+        size--;
+        return true;
+    }
+
+    public T get(int index) {  // Gets the object at an index
+        validIndex(index);
+        Node<T> current = head;
+        int i = 0;
+        while (i < index) {
+            current = current.getNext();
+            i++;
+        }
+        return current.getValue();
+    }
+
+    public T set(int index, Object element) { // Sets the value of a node in the list at the given index to the element
+        validIndex(index);
+        Node<T> current = head;
+        T prevValue = null;
+        int i = 0;
+        if(index == 0) {
+            prevValue = head.getValue();
+            head.setValue((T) element);
+        } else {
+            while(current != null) {
+                if(i == index) {
+                    prevValue = current.getValue();
+                    current.setValue((T) element);
+                    return prevValue;
+                }
+                current = current.getNext();
+                i++;
+            }
+        }
+
+        return prevValue;
+    }
+
+    public void add(int index, Object element) { // Adds an object at a given element to the linked list
+        if(index > size) {
+            validIndex(index);
+        }
+        Node<T> current = head;
+        int i = 0;
+        if(index == 0) {
+            if(isEmpty()) {
+                add(element);
+                return;
+            } else {
+                Node<T> newNode = new NodeImpl<T>((T) element, head.getNext());
+                head = newNode;
+                size++;
+                return;
+            }
+
+        }  else if(index == size) {
+            add(element);
+            return;
+        }
+        while(current != null) {
+            if(i == (index - 1)) {
+                Node<T> temp = current.getNext();
+                Node<T> newNode = new NodeImpl<T>((T) element, temp);
+                current.setNext(newNode);
+                size++;
+                return;
+            } else {
+                current = current.getNext();
+                i++;
+            }
+        }
+    }
+
+    public int indexOf(Object element) { // Gets index of an object in the linked list
+        Node<T> current = head;
+        int index = 0;
+        while(current != null) {
+            if(current.getValue().equals((T) element)) {
+                return index;
+            }
+            index++;
+            current = current.getNext();
+        }
+        return -1;
+    }
+
+    public int lastIndexOf(Object element) { // Gets the last index an object occurs at in the linked list
+        Node<T> current = head;
+        int index = -1;
+        int i = 0;
+        while(current != null) {
+            if(current.getValue().equals ((T) element)) {
+                index = i;
+            }
+            i++;
+            current = current.getNext();
+        }
+        return index;
+    }
+
+    public void validIndex(int i) { // Checks if index is in range
+        if(i < 0 || i >= size) {
+            throw new IndexOutOfBoundsException("Invalid index");
+        }
+    }
+    public Node<T> getHead() { // Gets head
+        return head;
+    }
+
+    @Override
+    public String toString() {
+        String list = "";
+        Node<T> current = head;
+        while(current != null) {
+            if(current.getNext() == null)
+                list+= current.getValue();
+            else
+                list += current.getValue() + " -> ";
+            current = current.getNext();
+        }
+        return list;
+    }
+}
